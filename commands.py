27c1fbc41abb43505a592db743d3aa13b7f48f02
GreyAlien502 <carlos.a@del-castillo.net>
2015-11-16 05:41:02 +0000

    so many things

--- commands.py
+++ commands.py
@@ -2,19 +2,36 @@ import time
 import os
 import json
 
+import ezemail
+
 def folder():
 	return os.getenv('HOME')+"/zerved/"
+def readJSON(filename):
+	f = open(filename,'r')
+	out = json.load(f)
+	f.close()
+	return out
+def writeJSON(filename,content):
+	f = open(filename,'w')
+	out = f.write(json.dumps(content))
+	f.close()
+	return out
 
 def checkCreds(username,password):
 	userfile = open(folder()+"accounts",'r')
 	credlist = userfile.read().splitlines()
+	if not username: username = ''
+	if not password: password = ''
 	if(username+'|'+password in credlist):
 		return True
 	else:
 		return False
 
 def prefs(username):
-	return json.load(open(folder()+"user/"+username+'/preferences'))
+	return readJSON(folder()+"user/"+username+'/preferences')
+def message(username,content):
+	address = prefs(username)['message address']
+	ezemail.sendEmail()
 
 def login(contents):
 	return '<h1>Welcome. Enjoy your premium <a href="content.html#posts">membership.</a></h1>'
@@ -22,46 +39,40 @@ def home(contents):
 	return 'Welcome '+contents['username']+'.'
 
 def posts(contents):
-	postnames = os.listdir(folder()+'posts');
+	postnames = os.listdir(folder()+'posts')
 	postnames.sort(reverse=True)
 	start = contents['data']['start']
-	length = prefs(contents['username'])['postnumber']
+	length = int(prefs(contents['username'])['postnumber'])
 	if start+length > len(postnames):
 		toSend = postnames[start:]
 	else:
 		toSend = postnames[start:start+length]
-	toSend = [json.load(open(folder()+'posts/'+postname)) for postname in toSend]
+	toSend = [readJSON(folder()+'posts/'+postname) for postname in toSend]
 	return {'posts':toSend,'start':start}
 def post(contents):
 	post = contents['data']
 	posttime = time.time()
 	post.update({'time':posttime,'author':contents['username'],'comments':[]})
-	post = json.dumps(post)
-	postfile = open(folder()+'posts/'+str(posttime),'w')
-	postfile.write(post)
-	postfile.close()
-	commentfile = open(folder()+'comments/'+str(posttime),'w')
-	commentfile.write('{"comments":[]}')
-	commentfile.close()
+	writeJSON(folder()+'posts/'+str(posttime),post)
+	writeJSON(folder()+'comments/'+str(posttime),{"comments":[]})
 	return 'Sent!'
+	
 
 def comments(contents):
 	postnum = str(contents['data']['post'])
-	post     = json.load(open(folder()+'posts/'   +postnum))
-	comments = json.load(open(folder()+'comments/'+postnum))
+	post     = readJSON(folder()+'posts/'   +postnum)
+	comments = readJSON(folder()+'comments/'+postnum)
 	comments = comments['comments']
-	return {'post':post,'comments':comments} 
+	return {'post':post,'comments':comments}
 def comment(contents):
 	posttime = time.time()
 	commentfilename = folder()+'comments/'+str(contents['data']['post'])
-	comments = json.load(open(commentfilename))
+	comments = readJSON(commentfilename)
 	comment = comments
 	for i in contents['data']['comment']:
 		comment = comment['comments'][i]
 	comment['comments'].append({'time':posttime,'author':contents['username'],'content':contents['data']['content'],'comments':[]})
-	commentfile = open(commentfilename,'w')
-	commentfile.write(json.dumps(comments))
-	commentfile.close()
+	writeJSON(commentfilename, comments)
 	return {'message':'Commented!','post':contents['data']['post']}
 
 def files(contents):
@@ -88,6 +99,14 @@ def file(contents):
 def preferences(contents):
 	return prefs(contents['username'])
 
-def cat(contents):
-	to = contents['to']
+def preference(contents):
+	preffile = folder()+'user/'+contents['username']+'/preferences'
+	writeJSON(preffile,contents['data'])
+	return 'success'
 
+
+def cat(contents):
+	to = contents['data']['to']
+	catfile = open(folder()+'cats/clients','a')
+	catfile.write(to+'\n')
+	return 'success'
