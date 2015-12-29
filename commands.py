730ad87458f4e16cbb0d3170453142e3dcf48331
GreyAlien502 <carlos.a@del-castillo.net>
2015-12-29 08:09:59 +0000

    basically added all the file commands and debugged

--- commands.py
+++ commands.py
@@ -55,10 +55,13 @@ def comment(contents):
 	writeJSON(commentfilename, {'comments':comments})
 	return {'message':'Commented!','post':contents['data']['post']}
 
-def files(contents):
-	subpath = contents['data']['path']
-	path = folder()+'user/'+contents['username']+'/files/'+ subpath
+def checkPath(path):
 	if '..' in path: return 'You cant have a \'..\' in a filename, got it?'
+	
+def files(contents):
+	subpath = '/'.join(contents['data']['path'])
+	path = folder()+'user/'+contents['username']+'/files/'+ subpath
+	if checkPath(path):return checkPath(path)
 	if os.path.isdir(path):
 		filetype = 'directory'
 		return {'type':'directory','files':os.listdir(path),'path':subpath}
@@ -67,6 +70,46 @@ def files(contents):
 		content = actuafile.read()
 		actuafile.close()
 		return{'type':'file','content':content,'path':subpath}
+def addFile(contents):
+	path = folder()+'user/'+contents['username']+'/files/'+ '/'.join(contents['data']['path'])
+	if checkPath(path):return checkPath(path)
+	def checkFilename(name):
+		alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~'
+		for char in name:
+			if char not in alphabet:
+				return False
+		return True
+	if not checkFilename(path.split('/')[-1]): return 'bad name'
+	if contents['data']['type'] == 'file':
+		open(path,'w').close()
+	else:
+		os.mkdir(path)
+	return 'success'
+def removeFile(contents):
+	path = folder()+'user/'+contents['username']+'/files/'+ '/'.join(contents['data']['path'])
+	if checkPath(path):return checkPath(path)
+	if os.path.isdir(path):
+		os.rmdir(path)
+	else:
+		os.remove(path)
+	return 'success'
+def renameFile(contents):
+	path = folder()+'user/'+contents['username']+'/files/'+ '/'.join(contents['data']['path'])
+	if checkPath(path):return checkPath(path)
+	newpath = '/'.join(path.split('/')[:-1]+ [contents['data']['newname']])
+	if checkPath(newpath):return checkPath(newpath)
+	os.rename(path,newpath)
+	return {'path':'/'.join(contents['data']['path'][:-1]+ [contents['data']['newname']])}
+def editFile(contents):
+	path = folder()+'user/'+contents['username']+'/files/'+ '/'.join(contents['data']['path'])
+	if checkPath(path):return checkPath(path)
+	data = contents['data']['data']
+	print(path)
+	f=open(path,'w')
+	f.write(data)
+	f.close()
+	return 'success'
+
 def file(contents):
 	subpath = contents['data']['path']+contents['data']['title']
 	content = contents['data']['content']
