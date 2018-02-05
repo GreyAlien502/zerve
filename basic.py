eb0b4eed945d7821f72fbdc7d6a8d91e5855cd4f
GreyAlien502 <greyalien502@gmail.com>
2018-02-05 10:48:39 -0500

    changing most services to sql

--- basic.py
+++ basic.py
@@ -2,9 +2,9 @@ import time
 import os
 import json
 
+
 def folder():
-	return '/home/pi/zerved/'
-	#return os.getenv('HOME')+"/zerved/"
+	return os.getenv('HOME')+"/zerved/"
 def readJSON(filename):
 	f = open(filename,'r')
 	out = json.load(f)
@@ -16,20 +16,6 @@ def writeJSON(filename,content):
 	f.close()
 	return out
 
-def checkCreds(username,password):
-	credlist = readJSON(folder()+"accounts")
-	if not username: username = ''
-	if not password: password = ''
-	if({'username':username,'password':password} in credlist):
-		return True
-	else:
-		return False
-
-def prefs(username):
-	return readJSON(folder()+"user/"+username+'/preferences')
-
-def login(contents):
-	return {'status':0,'message':'<h1>Welcome. Enjoy your premium <a href="content.html#posts">membership.</a></h1>'}
 def home(contents):
 	return 'Welcome '+contents['username']+'.'
 
